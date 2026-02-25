e99ff3aefd83a2590812fddd23c99a4bb60f261c
raghav bhardwaj <braghav474@gmail.com>
2026-02-25 11:14:30 +0530

    implemented token

--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from 'mongoose'
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { v4 as uuidv4 } from 'uuid';
 
 const userSchema = new Schema(
     {
@@ -71,12 +73,77 @@ userSchema.pre("save",async function () {
    const salt = await bcrypt.genSalt(10)
    this.password = await bcrypt.hash(this.password,salt)
 })
-
+   
 // compare the password
 userSchema.methods.isPasswordCorrect = async function (incomingPassword) {
     return  bcrypt.compare(incomingPassword,this.password)
 }
 
+// generate access token => jwt - payload - data {} , secret key , expiresIn
+userSchema.methods.generateAccessToken = async function () {
+    try {
+       const token = await jwt.sign(
+            {
+               id:this._id,
+               email:this.email,
+               userName:this.userName
+            }
+            ,process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+            })
+
+            return token
+    } catch (error) {
+        console.log(error,"error generating AT");
+    }
+}
+
+// refresh token
+userSchema.methods.generateRefreshToken = async function () {
+     try {
+       const token =  await jwt.sign({
+           id:this._id
+        },process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn:process.env.REFRESH_TOKEN_EXPIRY})
+        return token
+     } catch (error) {
+        console.log(error,"error generating refresh token");
+        
+     }
+}
+
+// email verification token
+userSchema.methods.generateEmailVerificationToken = async function () {
+     try {
+        // generate a random token
+        const rawToken = uuidv4()
+        // hash token
+        this.emailVerificationToken =  await bcrypt.hash(rawToken,10)
+        this.emailVerificationTokenExpiry = Date.now() + 24*60*60*1000
+
+        return rawToken
+     } catch (error) {
+        console.log(error,"error generating email verification token");
+     }
+}
+
+// forgot pass token
+userSchema.methods.generateForgotPasswordToken = async function () {
+     try {
+        // generate a random token
+        const rawToken = uuidv4()
+        // hash token
+        this.forgotPasswordToken =  await bcrypt.hash(rawToken,10)
+        this.forgotPasswordTokenExpiry = Date.now() + 20*60*1000
+
+        return rawToken
+     } catch (error) {
+        console.log(error,"error generating forgot password token");
+     }
+}
+
+// raghav => raghav
 
 const User = mongoose.model('User', userSchema)
 
